ec1860e427b1e277a6de8018a3264b339df0926a
changjinqi1 <160363438+changjinqi1@users.noreply.github.com>
2025-04-14 21:28:30 -0700

    Player Warning+GameOver

--- PrisonEscape/Assets/Kay/Scripts/PlayerState.cs
+++ PrisonEscape/Assets/Kay/Scripts/PlayerState.cs
@@ -1,13 +1,15 @@
 using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
 
 /// <summary>
 /// This script checks both players' states and determines if they are behaving or misbehaving.
 /// So, there should only be one instance of this script in the scene.
 /// </summary>
-
 public class PlayerState : MonoBehaviour
 {
     JailerBehavior _jailerBehavior; // reference to the jailer behavior script
+
     public enum PlayerStates
     {
         Behaving,
@@ -15,10 +17,19 @@ public class PlayerState : MonoBehaviour
     }
     public PlayerStates CurrentState;
 
+    public GameObject filterUI;      // reference to UI overlay shown when caught
+    public GameObject gameOverPanel; // reference to UI game over panel
+
+    private int timesCaught = 0;
+    private bool wasMisbehavingLastFrame = false;
+
     private void Start()
     {
         _jailerBehavior = FindAnyObjectByType<JailerBehavior>(); // find the jailer behavior script in the scene
         CurrentState = PlayerStates.Behaving; // set initial state
+
+        if (filterUI != null) filterUI.SetActive(false);
+        if (gameOverPanel != null) gameOverPanel.SetActive(false);
     }
 
     private void Update()
@@ -27,18 +38,41 @@ public class PlayerState : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.B))
         {
             if (CurrentState == PlayerStates.Behaving)
-            {
                 ChangeState(PlayerStates.Misbehaving);
-            }
-            else if (CurrentState == PlayerStates.Misbehaving)
-            {
+            else
                 ChangeState(PlayerStates.Behaving);
-            }
         }
 
         if (Input.GetKeyDown(KeyCode.D)) // Distract jailer
         {
             _jailerBehavior.DistractJailerFor(5f); // distract the jailer for 5 seconds
+        }
+
+        // Catching logic
+        if (_jailerBehavior.CurrentState == JailerBehavior.JailerState.Alert &&
+            CurrentState == PlayerStates.Misbehaving)
+        {
+            if (!wasMisbehavingLastFrame) // only trigger once per catch
+            {
+                timesCaught++;
+                Debug.Log("Player caught! Times caught: " + timesCaught);
+
+                if (filterUI != null)
+                    filterUI.SetActive(true); // show red filter
+
+                if (timesCaught >= 2 && gameOverPanel != null)
+                {
+                    gameOverPanel.SetActive(true); // show game over
+                }
+
+                StartCoroutine(HideFilterAfterDelay(1.5f)); // hide red filter after short delay
+            }
+
+            wasMisbehavingLastFrame = true;
+        }
+        else
+        {
+            wasMisbehavingLastFrame = false;
         }
     }
 
@@ -49,6 +83,13 @@ public class PlayerState : MonoBehaviour
      * also be sure to check if the player is touching things within the jail cell---if they are, that also counts as misbehavior
      */
     #endregion
+
+    private IEnumerator HideFilterAfterDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        if (filterUI != null)
+            filterUI.SetActive(false);
+    }
 
     private void ChangeState(PlayerStates newState)
     {
