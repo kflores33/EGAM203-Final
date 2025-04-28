62d5ef6c7a2de58832eda6081f1ecdf30ec91320
kflores33 <kflores3@inside.artcenter.edu>
2025-04-27 21:13:35 -0700

    final

--- PrisonEscape/Assets/Kay/Scripts/CodeInput.cs
+++ PrisonEscape/Assets/Kay/Scripts/CodeInput.cs
@@ -1,27 +1,40 @@
-using UnityEngine;
+﻿using UnityEngine;
 using System.Collections.Generic;
+using TMPro;
 
 public class CodeInput : MonoBehaviour
 {
     public BasicGM gameManager;
 
-    
-    public List<char> CorrectCode;
+
+    public List<char> CorrectCode = new List<char> { 'c', 'b', 'd', 'a' };
 
     public List<char> PlayerInput = new List<char>();
-    private void Start()
+
+    private HashSet<char> allowedLetters = new HashSet<char> { 'c', 'b', 'd', 'a' };
+
+    public TMP_Text inputDisplayText;
+
+    private Dictionary<char, string> symbolMapping = new Dictionary<char, string>
     {
-        CorrectCode = new List<char> { 'c', 'b', 'd','a' };
-    }
+        { 'c', "■" },
+        { 'b', "▲" },
+        { 'd', "★" },
+        { 'a', "⚪" }
+    };
 
     private void Update()
     {
-        
         for (KeyCode key = KeyCode.A; key <= KeyCode.Z; key++)
         {
             if (Input.GetKeyDown(key))
             {
-                AddToInput((char)key);
+                char inputChar = char.ToLower((char)key);
+
+                if (allowedLetters.Contains(inputChar))
+                {
+                    AddToInput(inputChar);
+                }
                 break;
             }
         }
@@ -30,7 +43,9 @@ public class CodeInput : MonoBehaviour
     public void AddToInput(char letter)
     {
         PlayerInput.Add(letter);
-        Debug.Log($"Added letter: {letter}");
+        Debug.Log($"[Input] Added: {letter}");
+
+        UpdateInputDisplay();
 
         if (PlayerInput.Count == CorrectCode.Count)
         {
@@ -44,15 +59,45 @@ public class CodeInput : MonoBehaviour
         {
             if (PlayerInput[i] != CorrectCode[i])
             {
-                Debug.Log("Incorrect code");
+                Debug.Log("[Check] Incorrect code. Try again.");
                 PlayerInput.Clear();
+                UpdateInputDisplay();
                 return;
             }
         }
 
-        Debug.Log("Correct code --- You escaped!!");
+        Debug.Log("[Check] Correct code --- You escaped!!");
         gameManager.ShowEndScreen(true);
         PlayerInput.Clear();
+        UpdateInputDisplay();
+    }
+
+    private void UpdateInputDisplay()
+    {
+        if (inputDisplayText == null) return;
+
+        if (PlayerInput.Count == 0)
+        {
+            inputDisplayText.text = "Code:";
+        }
+        else
+        {
+            inputDisplayText.text = "Code:";
+
+            for (int i = 0; i < PlayerInput.Count; i++)
+            {
+                if (symbolMapping.ContainsKey(PlayerInput[i]))
+                {
+                    inputDisplayText.text += symbolMapping[PlayerInput[i]];
+                }
+                else
+                {
+                    inputDisplayText.text += PlayerInput[i];
+                }
+
+                //if (i != PlayerInput.Count - 1)
+                    //inputDisplayText.text += " → ";
+            }
+        }
     }
 }
-
